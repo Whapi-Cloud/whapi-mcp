4d7679719aec5ad2b9858e196f21e440e03374b7
Whapi-Cloud <care@whapi.cloud>
2026-08-28 16:20:14 +0300

    Release v0.0.21

--- generated-mcp/updateChannelSettings.js
+++ generated-mcp/updateChannelSettings.js
@@ -40,6 +40,7 @@ module.exports = async function updateChannelSettings(args, env = process.env) {
     if (args.hasOwnProperty('offline_mode')) bodyObj['offline_mode'] = args['offline_mode'];
     if (args.hasOwnProperty('full_history')) bodyObj['full_history'] = args['full_history'];
     if (args.hasOwnProperty('auto_read_messages')) bodyObj['auto_read_messages'] = args['auto_read_messages'];
+    if (args.hasOwnProperty('outgoing_calls_enabled')) bodyObj['outgoing_calls_enabled'] = args['outgoing_calls_enabled'];
     if (args.hasOwnProperty('locale')) bodyObj['locale'] = args['locale'];
     if (args.hasOwnProperty('mock_pairing_code')) bodyObj['mock_pairing_code'] = args['mock_pairing_code'];
     if (args.hasOwnProperty('ignored_presences')) bodyObj['ignored_presences'] = args['ignored_presences'];
